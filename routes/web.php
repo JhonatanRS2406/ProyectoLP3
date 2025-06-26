9da6ef155bac2b2965a1b82fc302454a299bf25f
Jhonatan Rucabado Suasnabar <2020210349@udh.edu.pe>
2025-06-26 16:59:01 -0500

    Actualizacion

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,27 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Ususario//
+// Ususario //
 
 Route::get("/usuario/miperfil",function(){
     return view("usuario.verUsuario");
 });
 
-Route::post("/usuario/crear",function(){
+Route::get("/usuario/crear",function(){
     return view("usuario.crearUsuario");
 });
 
+Route::get("/usuario/actualizar",function(){
+    return view("usuario.actualizarUsuario");
+});
+
+Route::put("/usuario/guardar",function(){
+            return view("usuario.crearUsuario");
+});
 
 // Tareas //
 
-Route::post("usuario/tarea/crear",function(){
+Route::get("usuario/tarea/crear",function(){
     return view("tarea.crearTarea");
 });
 
@@ -32,8 +39,26 @@ Route::get("/usuario/tarea/ver",function(){
     return view("tarea.verTarea");
 });
 
+Route::get("/usuario/tarea/modificar",function(){
+    return view("tarea.actualizarTarea");
+});
+
+Route::put("/usuario/tarea/guardar",function(){
+            return view("tarea.crearTarea");
+});
+
 // Actividad //
 // Notificacion //
+Route::get("/usuario/notificacion",function(){
+    return view("notificacion.verNotificacion");
+});
 // FAQ //
+Route::get("/FAQ",function(){
+    return view("faq.verFAQ");
+});
 // Reporte //
+
+Route::get("/usuario/reporte",function(){
+    return view("reporte.verReporte");
+});
 ////
